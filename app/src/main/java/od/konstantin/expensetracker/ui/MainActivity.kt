411e0b1f8f45e5109d552b6bd71aecc0ad0120b4
Константин Клищук <kostyak13k@gmail.com>
2021-06-22 14:52:00 +0300

    init commit

---
+++ app/src/main/java/od/konstantin/expensetracker/ui/MainActivity.kt
@@ -0,0 +1,6 @@
+package od.konstantin.expensetracker.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import od.konstantin.expensetracker.R
+
+class MainActivity : AppCompatActivity(R.layout.activity_main)
