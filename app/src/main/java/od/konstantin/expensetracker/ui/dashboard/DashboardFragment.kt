8cd2c56e4d07fb3f8424bb0367e44260baf085c9
Константин Клищук <kostyak13k@gmail.com>
2021-06-23 13:08:21 +0300

    added dashboard fragment

---
+++ app/src/main/java/od/konstantin/expensetracker/ui/dashboard/DashboardFragment.kt
@@ -0,0 +1,10 @@
+package od.konstantin.expensetracker.ui.dashboard
+
+import androidx.fragment.app.Fragment
+import od.konstantin.expensetracker.R
+
+
+class DashboardFragment : Fragment(R.layout.fragment_dashboard) {
+
+
+}
