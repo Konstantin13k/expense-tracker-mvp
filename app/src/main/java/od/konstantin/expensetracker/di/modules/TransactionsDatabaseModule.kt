36897fabb4edd2a7b294e0756337d2f03bcb5a5d
Константин Клищук <kostyak13k@gmail.com>
2021-06-24 10:35:33 +0300

    added database module

---
+++ app/src/main/java/od/konstantin/expensetracker/di/modules/TransactionsDatabaseModule.kt
@@ -0,0 +1,30 @@
+package od.konstantin.expensetracker.di.modules
+
+import android.content.Context
+import androidx.room.Room
+import dagger.Module
+import dagger.Provides
+import od.konstantin.expensetracker.data.local.TransactionsDatabase
+import od.konstantin.expensetracker.data.local.transactions.dao.TransactionsDao
+import javax.inject.Singleton
+
+private const val DATABASE_NAME = "transactions.db"
+
+@Module
+class TransactionsDatabaseModule {
+
+    @Provides
+    @Singleton
+    fun provideTransactionsDatabase(context: Context): TransactionsDatabase {
+        return Room.databaseBuilder(
+            context,
+            TransactionsDatabase::class.java,
+            DATABASE_NAME
+        ).build()
+    }
+
+    @Provides
+    fun provideTransactionsDao(database: TransactionsDatabase): TransactionsDao {
+        return database.transactionsDao
+    }
+}
