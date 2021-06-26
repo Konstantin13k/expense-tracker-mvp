589767db5eeed46f73e96aeea00e41712464cf9e
Константин Клищук <kostyak13k@gmail.com>
2021-06-26 20:55:59 +0300

    added transaction details fragment

---
+++ app/src/main/java/od/konstantin/expensetracker/ui/details/TransactionDetailsFragment.kt
@@ -0,0 +1,7 @@
+package od.konstantin.expensetracker.ui.details
+
+import androidx.fragment.app.Fragment
+import od.konstantin.expensetracker.R
+
+class TransactionDetailsFragment : Fragment(R.layout.fragment_transaction_details) {
+}
